145aac5d90c264b14ad48e3e4145b3aa6f5c7180
bmiit145 <21bmiit145@gmail.com>
2022-07-26 10:27:16 +1000

    2-08_sec_commit

--- pr_1_10.cpp
+++ pr_1_10.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     string a;
-    int len = 0;
+    int len = 0 , check = 0 ;
     cout << "\tEnter the Any string" << endl;
     cin >> a;
 
@@ -15,12 +15,18 @@ int main()
         if (a[i] != a[len-1-i])
         {
             cout << "\n GIven string is not palindrome \n";
+            check = 1;
             break;
-        }else{
-            cout << "\nGiven string is palindrome\n"
         }
-        
     }
+    if (check !=1)
+    {
+        cout << "\nGiven string is palindrome\n";
+    }
+    
+
+
+
     
 
 
